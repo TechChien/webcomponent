6482eaef8592fdf855b70bb10485623fe7d8da1c
redchien <red.chien45697@gmail.com>
2024-02-05 23:15:34 +0800

    custom html first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+const template = document.createElement("template");
+template.innerHTML = `
+  <div id='root'>
+    <input type="text" id="inputDate" class="form-control" placeholder="yyyy/mm/dd">
+  </div>
+`;
+
+class CustomDatePicker extends HTMLElement {
+  constructor() {
+    super();
+    this.root = this.attachShadow({ mode: "open" });
+    // shadowRoot shields the web component from external styling, mostly
+    let clone = template.content.cloneNode(true);
+    this.root.append(clone);
+  }
+
+  static observedAttributes = ["lan"];
+
+  connectedCallback() {
+    this.init(this.getAttribute("lan"));
+    this._value = "";
+  }
+
+  init(lan) {
+    this.customInput = this.root.querySelector("#inputDate");
+    $(this.customInput).datepicker({
+      language: lan, //设置语言
+      autoclose: true, //选择后自动关闭
+      clearBtn: true, //清除按钮
+      format: "yyyy-mm-dd", //日期格式
+    });
+    this._value = "";
+  }
+
+  get value() {
+    return $(this.customInput)
+      .data("datepicker")
+      .getFormattedDate("yyyy-mm-dd");
+  }
+
+  set value(v) {
+    this._value = v;
+  }
+
+  attributeChangedCallback(name, _, newLan) {
+    if (name === "lan") {
+      $(this.root.querySelector("#inputDate")).datepicker("destroy");
+      this.init(newLan);
+    }
+  }
+}
+
+customElements.define("input-date", CustomDatePicker);
